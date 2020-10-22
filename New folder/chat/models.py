bb8754c2d5621d56a5c4215ce81bdc0d5cb1b767
sunjol <sunjol123456789@gmail.com>
2020-10-22 09:36:30 +0530

    first

---
+++ New folder/chat/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from django.contrib.auth import get_user_model
+from django.db import models
+from account.models import Account
+
+# Create your models here.
+User = Account
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_10_messages():
+        return Message.objects.order_by('-timestamp').all()[:10]
+# Create your models here.
---
+++ New folder/chat1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Chat1Config(AppConfig):
+    name = 'chat1'
